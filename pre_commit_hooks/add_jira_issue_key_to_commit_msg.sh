55558ac3b9f07390b4ecd7028ab14f0b1936aee9
Hyeonki Hong <hyeonki.hong@moreh.io>
2024-08-16 12:08:40 +0900

    MAF-13948: refactor(jira): use getopt to get commit msg file name

--- pre_commit_hooks/add_jira_issue_key_to_commit_msg.sh
+++ pre_commit_hooks/add_jira_issue_key_to_commit_msg.sh
@@ -2,18 +2,12 @@
 
 set -e
 
-eval COMMIT_MSG_FILE=\"\${$#}\"
-
-options=$(getopt -o k: -l key: -n $(basename "$0") -- "$@")
+options=$(getopt -o k: -l key: -n "$(basename "$0")" -- "$@")
 
 eval set -- "$options"
 
 while true; do
     case "$1" in
-    msg)
-        COMMIT_MSG=$2
-        shift
-        ;;
     -k | --key)
         if [ -z "$PROJECT_KEYS" ]; then
             PROJECT_KEYS="$2"
@@ -23,7 +17,8 @@ while true; do
         shift 2
         ;;
     --)
-        shift
+        COMMIT_MSG_FILE=$2
+        shift 2
         break
         ;;
     *)
